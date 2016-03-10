fa176d9c6a845637104d051ec321ae88430b4611
Chris-Fitzpatrick <chrishfitzpatrick@gmail.com>
2016-03-09 19:11:06 -0500

    made it work locally

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,34 @@
-// Your JavaScript goes here...
+function parse(){
+
+        request = new XMLHttpRequest();
+
+        request.open("GET", "https://messagehub.herokuapp.com/messages.json", true);
+
+        request.onreadystatechange = makeCall;
+
+        request.send(null);
+        console.log("at 10");
+        
+
+        function makeCall (){
+                console.log("at 12");
+
+                if (request.readyState == 4 && request.status == 200){
+                        console.log("at 14");
+                        result = "";
+                        rawResponse = request.responseText;
+                        messageData = JSON.parse(rawResponse);
+                        elements = document.getElementById("messages");
+
+                        for (i = 0; i < 2; i++){
+                                result += "<p>" + messageData[i]["content"] + " from: " + messageData[i]["username"] + "</p>";
+                        }
+
+                        elements.innerHTML = result;
+                }
+
+                else if (request.readyState == 4 && request.status != 200) {
+                                document.getElementById("messages").innerHTML = "<p>broken :/</p>";
+                }
+        }
+}
